015a2a65ba96db24dffb6aef4dc8f9a1e9dae452
unknown <verezh@gmail.com>
2022-08-30 23:01:24 +0300

    up

--- sample/src/app/index.tsx
+++ sample/src/app/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './index.scss';
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -23,4 +22,3 @@ function App() {
   );
 }
 
-export default App;
