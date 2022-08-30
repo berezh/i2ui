b391b02c754f3098e79b7b72ce2ee0cc82cce7bf
unknown <verezh@gmail.com>
2022-08-30 23:41:10 +0300

    u

--- ucase/src/app/index.tsx
+++ ucase/src/app/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Counter } from '../features/counter/Counter';
+import './index.scss';
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -54,5 +52,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
