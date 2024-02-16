01f2c2931123ded720b01896b6fdca1ee5ed1732
Berezh <verezh@gmail.com>
2024-02-16 03:02:26 +0200

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,6 @@ module.exports = {
   extends: ["varp"],
   rules: {
     /* Eslint rules */
-    "import/order": "off",
-    "import/namespace": "off",
-    "import/no-unresolved": "off",
     "import/no-default-export": "off",
   },
 };
